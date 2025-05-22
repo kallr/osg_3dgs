98475a589d31b44e750367dacca19914996606ff
kallr <kallr@163.com>
2025-05-22 10:55:32 +0800

    add cmakefiel

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+#include <osgViewer/Viewer>
+
 int main(){
     std::cout<<"my osg 3sgs renderer"<<std::endl;
+
+    osgViewer::Viewer viewer;
+    viewer.run();
+
+    return 0;
 }
